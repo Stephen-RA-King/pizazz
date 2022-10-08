9df6b745fa958f34bdfedf186f1d6b0cd1ac4403
Stephen-RA-King <33905365+Stephen-RA-King@users.noreply.github.com>
2022-10-08 17:33:14 +0100

    put version in other files

--- src/pizazz/__init__.py
+++ src/pizazz/__init__.py
@@ -1,6 +1,7 @@
 """Top-level package for pizazz."""
 
 __title__ = "pizazz"
+__version__ = "1.0.2"
 __author__ = "Stephen R A King"
 __description__ = (
     "Easy configuration and control of 74HC595 Shift Registers on a Raspberry Pi"
